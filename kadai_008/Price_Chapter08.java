15ffe79b0f08c4874ac5af73dabad742dc0d13aa
RzMyTy <mfytui@gmail.com>
2023-10-26 20:57:03 +0900

    kadai_008re

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -4,19 +4,20 @@ public class Price_Chapter08 {
 	public static void main(String[] args) {
 		
 		int userAge = 30;
-		int serviceCost = 3000;
+		String serviceCost = "";
 		
 		
-		switch(serviceCost) {
-		  case 10, 1000 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 20, 2000 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 30, 3000 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 40 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 50-> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 60, 4000 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 70 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-		  case 80, 5000 -> System.out.println( userAge + "代の料金は" + serviceCost + "円");
-	    default -> System.out.println("500円");
-		}
+		serviceCost = switch(userAge) {
+		  case 10 -> "1000円";
+		  case 20 -> "2000円";
+		  case 30 -> "3000円";
+		  case 40 -> "3000円";
+		  case 50 -> "4000円";
+		  case 60 -> "4000円";
+		  case 70 -> "4000円";
+		  case 80 -> "5000円";
+	    default -> "500円";
+		};
+		System.out.println( userAge + "代の料金は" + serviceCost);
 	}
 }
